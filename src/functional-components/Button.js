60b9282497af5b5fcf9355c3079e1a73ca0c8e1f
honestyman <leel50296@gmail.com>
2018-06-27 17:37:37 +0530

    Functional Components - Prop Types + Default Props examples

--- src/functional-components/Button.js
+++ src/functional-components/Button.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const BlueButton = (props) => {
     const styles = { background: 'blue', color: 'white' };
+    console.log('props.username : ', props.username);
 
     return <button {...props} style={styles}>Functional Component Button</button>
 }
+
+BlueButton.propTypes = {
+    username: PropTypes.string.isRequired
+};
+
+BlueButton.defaultProps = {
+    username: 'Zain'
+};
 
 const funcBtnClickHandler = (...options) => (e) => {
     console.log('event : ', e);
@@ -21,3 +31,11 @@ export const ButtonWithEvent = (props) => {
         >Functional Component Button with Event Handler</button>
     )
 }
+
+ButtonWithEvent.propTypes = {
+    name: PropTypes.string.isRequired
+};
+
+ButtonWithEvent.defaultProps = {
+    name: 'Zain'
+};
